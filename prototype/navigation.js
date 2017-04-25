6cb4d8a5c30b4d9b19d2d704a311b77b1199df86
enblythe <eblythe@kent.edu>
2017-04-25 19:56:10 -0400

    making site changes

--- prototype/navigation.js
+++ prototype/navigation.js
@@ -1,22 +1,29 @@
-// selected elements
-var navTrigger = document.getElementById('nav-trigger');
-var nav = document.getElementById('nav');
-var labels = document.getElementsByClassName('nav-label');
+var slideIndex = 1;
+showDivs(slideIndex);
 
-// Event Listening
-navTrigger.addEventListener('click', navToggle);
-window.addEventListener('resize', resize);
-
-
-function navToggle(e) {
-  var closed = (navTrigger.className.indexOf('close') > 0);
-  if(closed) {
-    navTrigger.className = 'nav-trigger open';
-    nav.className = 'out';
-  } else {
-    navTrigger.className = 'nav-trigger close';
-    nav.className = 'in';
-  }
+function plusDivs(n) {
+    showDivs(slideIndex += n);
 }
 
-window.onload = resize;
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+}
+
+
+$(document).ready(function() {
+  $('body').addClass('js');
+  var $menu = $('#menu'),
+    $menulink = $('.menu-link');
+
+$menulink.click(function() {
+  $menulink.toggleClass('active');
+  $menu.toggleClass('active');
+  return false;
+});});
